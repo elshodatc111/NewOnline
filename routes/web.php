135dc0a3c3fb52a5d73fdb9a4506c902da010c0b
elshodatc111 <elshodatc1116@gmail.com>
2024-05-20 22:13:06 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {return view('welcome');});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
